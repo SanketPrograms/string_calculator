c0e9991049b1618e381e5713cd7f260b3a0c5be8
ritukedar <ritujakedar13@gmmail.com>
2025-12-16 15:51:47 +0530

    Add supports any amount of numbers

--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,10 +3,10 @@ import 'package:string_calculator/utils/string_calculator.dart';
 
 void main() {
   group('StringCalculator', () {
-    test('returns sum of two comma separated numbers', () {
+    test('returns sum of multiple comma separated numbers', () {
       final calculator = StringCalculator();
 
-      expect(calculator.add('1,5'), 6);
+      expect(calculator.add('1,2,3,4'), 10);
     });
   });
 }
