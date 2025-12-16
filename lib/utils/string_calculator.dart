43df1af5b47f4157a26a4005493d77135d400585
ritukedar <ritujakedar13@gmmail.com>
2025-12-16 15:56:44 +0530

    Throw exception for negative numbers

--- lib/utils/string_calculator.dart
+++ lib/utils/string_calculator.dart
@@ -1,6 +1,8 @@
 class StringCalculator {
   int add(String numbers) {
-    if (numbers.isEmpty) return 0;
+    if (numbers.isEmpty) {
+      return 0;
+    }
 
     String delimiter = ',';
     String numbersPart = numbers;
@@ -11,7 +13,21 @@ class StringCalculator {
       numbersPart = parts.last;
     }
 
-    final values = numbersPart.replaceAll('\n', delimiter).split(delimiter);
-    return values.map(int.parse).reduce((a, b) => a + b);
+    final values = numbersPart
+        .replaceAll('\n', delimiter)
+        .split(delimiter);
+
+    final parsedNumbers = values.map(int.parse).toList();
+
+    final negativeNumbers =
+    parsedNumbers.where((number) => number < 0).toList();
+
+    if (negativeNumbers.isNotEmpty) {
+      throw Exception(
+        'negative numbers not allowed ${negativeNumbers.join(',')}',
+      );
+    }
+
+    return parsedNumbers.fold(0, (sum, number) => sum + number);
   }
 }
--- test/string_calculator_test.dart
+++ test/string_calculator_test.dart
@@ -3,10 +3,17 @@ import 'package:string_calculator/utils/string_calculator.dart';
 
 void main() {
   group('StringCalculator', () {
-    test('supports custom delimiter', () {
+    test('throws exception with all negative numbers', () {
       final calculator = StringCalculator();
 
-      expect(calculator.add('//;\n1;2'), 3);
+      expect(
+        () => calculator.add('1,-2,-3'),
+        throwsA(
+          predicate(
+            (e) => e.toString().contains('-2,-3'),
+          ),
+        ),
+      );
     });
   });
 }
